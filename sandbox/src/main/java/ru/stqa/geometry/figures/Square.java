8cc2a39750f97c4fc82ca835b7dd46af17ce58f8
Вера <vosadchikova@alfabank.ru>
2024-07-27 00:37:02 +0300

    Эксперименты с классами

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -1,16 +1,16 @@
 package ru.stqa.geometry.figures;
 
-public class Square {
-    public static void printSquareArea(double a) {
-        String text = String.format("Площадь квадрата со стороной %f = %f", a, area(a));
+public record Square(double side) {
+    public static void printSquareArea(Square s) {
+        String text = String.format("Площадь квадрата со стороной %f = %f", s.side, s.area());
         System.out.println(text);
     }
 
-    public static double area(double a) {
-        return a * a;
+    public double area() {
+        return this.side * this.side;
     }
 
-    public static double perimeter(double a) {
-        return 4 * a;
+    public double perimeter() {
+        return this.side * 4;
     }
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test;
 public class SquareTests {
     @Test
     void canCalculateArea() {
-        var result = Square.area(5.0);
+        var s = new Square(5.0);
+        double result = s.area();
         Assertions.assertEquals(25.0, result);
     }
 
     @Test
     void canCalculatePerimeter() {
-        Assertions.assertEquals(20.0, Square.perimeter(5.0));
+        Assertions.assertEquals(20.0, new Square(5.0).perimeter());
     }
 }
