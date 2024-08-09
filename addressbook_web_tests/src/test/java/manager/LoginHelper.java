4384b868d6772b9f74679b7547a0e7978b6ce2b1
Вера <vosadchikova@alfabank.ru>
2024-08-09 22:40:27 +0500

    Рефакторинг хелперов

--- addressbook_web_tests/src/test/java/manager/LoginHelper.java
+++ addressbook_web_tests/src/test/java/manager/LoginHelper.java
@@ -2,16 +2,15 @@ package manager;
 
 import org.openqa.selenium.By;
 
-public class LoginHelper {
-    private final ApplicationManager manager;
+public class LoginHelper extends HelperBase {
 
     public LoginHelper(ApplicationManager manager) {
-        this.manager = manager;
+        super(manager);
     }
 
     void login(String user, String pass) {
-        manager.driver.findElement(By.name("user")).sendKeys(user);
-        manager.driver.findElement(By.name("pass")).sendKeys(pass);
-        manager.driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        type(By.name("user"), user);
+        type(By.name("pass"), pass);
+        click(By.xpath("//input[@value=\'Login\']"));
     }
 }
