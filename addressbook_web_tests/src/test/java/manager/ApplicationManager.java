39dfd802de14cb3f198c25539d6e2501ec63a636
Вера <vosadchikova@alfabank.ru>
2024-08-09 21:19:03 +0500

    Добавление параметризации запуска по названию браузера

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -1,10 +1,10 @@
 package manager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
     protected WebDriver driver;
@@ -13,12 +13,17 @@ public class ApplicationManager {
 
     private GroupHelper groups;
 
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
-            driver = new ChromeDriver();
+            if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown Browser %s", browser));
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get("http://localhost/addressbook/index.php");
-            driver.manage().window().setSize(new Dimension(1636, 921));
             session().login("admin", "secret");
         }
     }
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -11,6 +11,6 @@ public class TestBase {
         if (app == null) {
             app = new ApplicationManager();
         }
-        app.init();
+        app.init(System.getProperty("browser", "chrome"));
     }
 }
