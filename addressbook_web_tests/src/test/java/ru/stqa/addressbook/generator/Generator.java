b102b5ee484586e12d044573eb394ac690a176f0
Вера <vosadchikova@alfabank.ru>
2024-09-02 00:28:58 +0500

    Сохранение в YAML

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
@@ -4,6 +4,7 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import ru.stqa.addressbook.common.Common;
 import ru.stqa.addressbook.model.GroupData;
 
@@ -45,6 +46,9 @@ public class Generator {
             try (var writer = new FileWriter(output)) {
                 writer.write(json);
             }
+        } else if ("yaml".equals(format)) {
+            var mapper = new YAMLMapper();
+            mapper.writeValue(new File(output), data);
         } else {
             throw new IllegalArgumentException("Неизвестный формат " + format);
         }
