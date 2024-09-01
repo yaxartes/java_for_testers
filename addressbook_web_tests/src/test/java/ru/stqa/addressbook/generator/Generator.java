7d1750e51dbf04d274d9382c4fc1288de766e05c
Вера <vosadchikova@alfabank.ru>
2024-09-02 00:41:32 +0500

    Работа с файлами XML

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/generator/Generator.java
@@ -4,6 +4,7 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import ru.stqa.addressbook.common.Common;
 import ru.stqa.addressbook.model.GroupData;
@@ -48,6 +49,9 @@ public class Generator {
             }
         } else if ("yaml".equals(format)) {
             var mapper = new YAMLMapper();
+            mapper.writeValue(new File(output), data);
+        } else if ("xml".equals(format)) {
+            var mapper = new XmlMapper();
             mapper.writeValue(new File(output), data);
         } else {
             throw new IllegalArgumentException("Неизвестный формат " + format);
