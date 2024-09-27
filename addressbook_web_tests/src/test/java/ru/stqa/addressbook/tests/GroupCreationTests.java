b1e844f263562fe8ac58cd22acd5f979ce13c6c8
Вера <vosadchikova@alfabank.ru>
2024-09-27 16:54:04 +0500

    Использование множеств в тестах

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -14,7 +14,9 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GroupCreationTests extends TestBase {
@@ -39,7 +41,7 @@ public class GroupCreationTests extends TestBase {
         return new ArrayList<>(List.of(new GroupData().withName("group name'")));
     }
 
-    public static Stream<GroupData> singleRandomGroup() {
+    public static Stream<GroupData> randomGroups() {
         Supplier<GroupData> randomGroup = () -> new GroupData()
                 .withName(Common.randomString(10))
                 .withHeader(Common.randomString(20))
@@ -48,23 +50,19 @@ public class GroupCreationTests extends TestBase {
     }
 
     @ParameterizedTest
-    @MethodSource("singleRandomGroup")
+    @MethodSource("randomGroups")
     public void canCreateGroup(GroupData group) {
         var oldGroups = app.hbm().getGroupList();
         app.groups().createGroup(group);
 
         var newGroups = app.hbm().getGroupList();
-        Comparator<GroupData> compareById = (o1, o2) -> {
-            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
-        };
-        newGroups.sort(compareById);
+        var extraGroups = newGroups.stream().filter(g -> ! oldGroups.contains(g)).toList();
+        var newId = extraGroups.get(0).id();
 
-        var maxId = newGroups.get(newGroups.size() - 1).id();
         var expectedList = new ArrayList<>(oldGroups);
-        expectedList.add(group.withId(maxId));
-        expectedList.sort(compareById);
+        expectedList.add(group.withId(newId));
 
-        Assertions.assertEquals(newGroups, expectedList);
+        Assertions.assertEquals(Set.copyOf(newGroups), Set.copyOf(expectedList));
     }
 
     @ParameterizedTest
--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.addressbook.tests;
 
+import ru.stqa.addressbook.common.Common;
 import ru.stqa.addressbook.model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Random;
+import java.util.Set;
 
 public class GroupModificationTests extends TestBase {
     @Test
@@ -21,16 +23,11 @@ public class GroupModificationTests extends TestBase {
         var oldGroups = app.hbm().getGroupList();
         var rnd = new Random();
         var index = rnd.nextInt(oldGroups.size());
-        var testData = new GroupData().withName("modified name");
+        var testData = new GroupData().withName(Common.randomString(7));
         app.groups().modifyGroup(oldGroups.get(index), testData);
         var newGroups = app.hbm().getGroupList();
         var expectedList = new ArrayList<>(oldGroups);
         expectedList.set(index, testData.withId(oldGroups.get(index).id()));
-        Comparator<GroupData> compareById = (o1, o2) -> {
-            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
-        };
-        newGroups.sort(compareById);
-        expectedList.sort(compareById);
-        Assertions.assertEquals(newGroups, expectedList);
+        Assertions.assertEquals(Set.copyOf(newGroups), Set.copyOf(expectedList));
     }
 }
