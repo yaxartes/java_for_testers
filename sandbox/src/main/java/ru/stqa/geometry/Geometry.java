17257bebe744f855f51ee5f166dd009706a1888b
Вера <vosadchikova@alfabank.ru>
2024-09-27 14:50:22 +0500

    Функции для работы с эелементами списка

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -3,14 +3,15 @@ package ru.stqa.geometry;
 import ru.stqa.geometry.figures.Rectangle;
 import ru.stqa.geometry.figures.Square;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Geometry {
     public static void main(String[] args) {
-        Square.printSquareArea(new Square(7.0));
-        Square.printSquareArea(new Square(5.0));
-
+        var squares = List.of(new Square(7.0), new Square(5.0));
+        squares.forEach(Square::printSquareArea);
         Rectangle.printRectangleArea(3.0, 5.0);
         Rectangle.printRectangleArea(7.0, 9.0);
-
     }
 
 }
